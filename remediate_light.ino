d612dde9f53b4567c86845bef01e751adb98b81e
ddoan30 <ddoan@redhat.com>
2026-04-09 13:07:32 -0400

    Create remediate_light.ino

---
+++ remediate_light.ino
@@ -0,0 +1,21 @@
+const int sensor = 8; 
+const int relay = 13; 
+
+int x = 0;
+int y = 0;
+int lastSensorStatus = 0;
+
+void setup() {
+  // sets relay pin 13 to output AND sensor pin 8 to input
+  pinMode(relay, OUTPUT); 
+
+  pinMode(sensor, INPUT); 
+}
+
+void loop(){
+  // remediate: if light not on, turn it on
+  if (digitalRead(relay) != LOW){
+    digitalWrite(relay, LOW);
+  }
+
+}
