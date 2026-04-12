85d732c8442b0c76b3637e07154434c084aab27d
ddoan30 <ddoan@redhat.com>
2026-04-12 17:55:18 -0400

    Update unsecured_scenario.ino

--- unsecured_scenario.ino
+++ unsecured_scenario.ino
@@ -10,12 +10,25 @@ void setup() {
   pinMode(relay, OUTPUT); 
 
   pinMode(sensor, INPUT); 
+
+  // open serial port
+  Serial.begin(9600);
 }
 
 void loop(){
   // create unsecured scenario
-  if (digitalRead(sensor) == HIGH){
-    digitalWrite(relay, HIGH);
+
+  if (digitalRead(relay) == LOW){
+      Serial.println("Service on");
   }
+
+
+  if (digitalRead(sensor) == HIGH){
+    digitalWrite(relay, HIGH); //this actually turns off the light
+    Serial.println("Service off");
+  }
+  
+  delay(100);
+
 
 }
