a0453db5a07378b6307b57b19ba53d3403eb51ec
ddoan30 <ddoan@redhat.com>
2026-04-12 18:00:11 -0400

    Update remediate_light.ino

--- remediate_light.ino
+++ remediate_light.ino
@@ -7,6 +7,8 @@ int lastSensorStatus = 0;
 
 void setup() {
   // sets relay pin 13 to output AND sensor pin 8 to input
+  Serial.begin(96000);
+  
   pinMode(relay, OUTPUT); 
 
   pinMode(sensor, INPUT); 
@@ -16,6 +18,7 @@ void loop(){
   // remediate: if light not on, turn it on
   if (digitalRead(relay) != LOW){
     digitalWrite(relay, LOW);
+    Serial.println("Fixed");
   }
 
 }
